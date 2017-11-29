0af3bfc09364bc80537716dc909993797cd54ae4
rahuljoshi-contact <rahuljoshi.contact@gmail.com>
2017-11-29 12:47:03 -0500

    RJs implementation

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    // using array bu substring
+    // var len = str.length -1;
+    // var r = '';
+    // for (i=0;i<=len;i++)
+    // {
+    //   r = r + str.substr(len-i,1);
+    // }
+
+    // using split, array and join with empty string
+    var res = str.split('');
+    res = res.reverse();
+    return res.join('');
+}
 
 module.exports = reverse;
