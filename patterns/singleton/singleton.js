67092a0f10860e4e001186d1218b0762966b1982
rahuljoshi-contact <rahuljoshi.contact@gmail.com>
2018-01-28 22:11:56 -0500

    two impl of singleton from linux vm

--- patterns/singleton/singleton.js
+++ patterns/singleton/singleton.js
@@ -3,25 +3,49 @@
 // console.log(factorial(6));
 
 
-Singleton =
-(
-  function ()
-  {
-    var instantiated;
-    function init()
-    {
-      return {
-        publicMethod: function () { console.log('hello world');},
-        publicProperty: 'test'
-      };
-    }
+// Singleton =
+// (
+//   function ()
+//   {
+//     var instantiated;
+//     function init()
+//     {
+//       return {
+//         publicMethod: function () { console.log('hello world');},
+//         publicProperty: 'test'
+//       };
+//     }
+//
+//     return {
+//       getInstance: function () {
+//         if (!instantiated){ instantiated = init(); }
+//         return instantiated ;
+//       }
+//     };
+//   }
+// )();
 
-    return {
-      getInstance: function () {if (!instantiated){ instantiated = init(); }
-        return instantiated ;
-      }
-    };
+Singleton = (function () {
+
+  var instance ;
+
+  function init(x){
+    var object = new Object();
+    console.log("instance created: " + x);
+    return object ;
   }
-)();
 
-Singleton.getInstance().publicMethod();
+  return {
+    getInstance: function (x) {
+      if (!instance){
+        instance = init(x);
+      }
+      return instance ;
+    }
+  };
+
+})();
+
+
+Singleton.getInstance(5);
+Singleton.getInstance(6);
